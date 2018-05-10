b16c3ecd9c34d57585ca292e2921b93e3999e511
to-kach <39141521+to-kach@users.noreply.github.com>
2018-05-10 11:35:21 +0900

    Create helloooooooooo.py

---
+++ helloooooooooo.py
@@ -0,0 +1,16 @@
+class Hello:
+    hello = 0
+    def __init__(self):
+        Hello.hello += 1
+    def hello_hello():
+        print("hell")
+        for i in range(hello):
+            print("o",end = "")
+
+hello1 = Hello()
+hello2 = Hello()
+hello3 = Hello()
+
+hello2.hello_hello()
+
+    
